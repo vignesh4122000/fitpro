c9a87bb740722f6ec46f3f7307eb440de6772f9f
vignesh4122000 <vignesh4122000@gmail.com>
2023-05-05 11:46:49 +0530

    firstcommit

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,16 +2,34 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { UserhomeComponent } from './userhome/userhome.component';
+import { RouterModule, Routes } from '@angular/router';
+import { materialize } from 'rxjs';
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from '../shared/shared.module';
 
 
+const route:Routes=[
 
+  {
+  path:"home",
+  component: HomeComponent,
+  children: [
+    {
+      path: "",
+      component: UserhomeComponent
+    },]
+  }]
 @NgModule({
   declarations: [
     HomeComponent,
     UserhomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(route),
+    MaterialModule,
+    SharedModule,
+    
   ]
 })
 export class UserModule { }
